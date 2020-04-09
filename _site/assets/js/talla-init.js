0234f11d69a017c05aa25181f181678a629dd633
Bob Bailey <bbailey@brightcove.com>
2020-04-09 16:56:21 -0400

    initial version of Auth0 doc.

--- _site/assets/js/talla-init.js
+++ _site/assets/js/talla-init.js
@@ -1,7 +1,7 @@
-var appID = "00c26be3-4421-41e6-bb00-1dee68740f67"; 
+var appID = ""; 
 // token for anonymous profile
 var token =
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODI4MDQzNDF9.RYf2x65FXvdDURzM h00i76KfUrYlsDLsw6mVuEv6zmE';
+'';
 // -----
   Talla.config = Talla.init(
     appID,
@@ -10,7 +10,7 @@ var token =
     Talla.OptionAnonymous(true),
     Talla.OptionThemeColor('rgb(243, 148, 36)'),
     Talla.OptionDisplayName('Search Brightcove Engage Docs'),
-    Talla.OptionKnowledgeGroups(['2564d71e-af2a-444c-9271-08c33aaffbff']),
+    Talla.OptionKnowledgeGroups(['']),
     Talla.OptionWideBeacon(true),
     Talla.OptionBeaconCTA("Search Brightcove Engage Docs"),
     Talla.OptionOpenIcon('https://learning-services-media.brightcove.com/doc-assets/site-assets/images/site/search.png'),
